9e4d7a0d69f4e5b4910ec8923e6c434e2ead6405
atpawanraj <atpawanraj@gmail.com>
2026-04-17 19:17:42 +0530

    Feature UC7

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,61 +1,80 @@
 public class OOPSBannerApp {
 
-    // Method for letter O
-    public static String[] getOPattern() {
-        return new String[]{
-            " *** ",
-            "*   *",
-            "*   *",
-            "*   *",
-            "*   *",
-            "*   *",
-            " *** "
-        };
+    static class CharacterPattern {
+        private char character;
+        private String[] pattern;
+
+        public CharacterPattern(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        public char getCharacter() {
+            return character;
+        }
+
+        public String[] getPattern() {
+            return pattern;
+        }
     }
 
-    // Method for letter P
-    public static String[] getPPattern() {
-        return new String[]{
-            "**** ",
-            "*   *",
-            "*   *",
-            "**** ",
-            "*    ",
-            "*    ",
-            "*    "
-        };
+    public static CharacterPattern getCharacterPattern(char ch) {
+
+        if (ch == 'O') {
+            return new CharacterPattern('O', new String[]{
+                    " ***** ",
+                    "*     *",
+                    "*     *",
+                    "*     *",
+                    "*     *",
+                    "*     *",
+                    " ***** "
+            });
+        }
+
+        if (ch == 'P') {
+            return new CharacterPattern('P', new String[]{
+                    "****** ",
+                    "*     *",
+                    "*     *",
+                    "****** ",
+                    "*      ",
+                    "*      ",
+                    "*      "
+            });
+        }
+
+        return new CharacterPattern('S', new String[]{
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        });
     }
 
-    // Method for letter S
-    public static String[] getSPattern() {
-        return new String[]{
-            " ****",
-            "*    ",
-            "*    ",
-            " *** ",
-            "    *",
-            "    *",
-            "**** "
-        };
+    public static void printBanner(String word) {
+
+        CharacterPattern[] letters = new CharacterPattern[word.length()];
+
+        for (int i = 0; i < word.length(); i++) {
+            letters[i] = getCharacterPattern(word.charAt(i));
+        }
+
+        for (int row = 0; row < 7; row++) {
+            StringBuilder line = new StringBuilder();
+
+            for (int i = 0; i < letters.length; i++) {
+                line.append(letters[i].getPattern()[row]).append("  ");
+            }
+
+            System.out.println(line);
+        }
     }
 
     public static void main(String[] args) {
-
-        String[] o = getOPattern();
-        String[] p = getPPattern();
-        String[] s = getSPattern();
-
-        // Loop through 7 rows
-        for(int i = 0; i < 7; i++) {
-
-            System.out.println(
-                String.join(" ",
-                    o[i],   // O
-                    o[i],   // O again
-                    p[i],   // P
-                    s[i]    // S
-                )
-            );
-        }
+        printBanner("OOPS");
     }
 }
